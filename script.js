a65095a0bad3b312f2006fee5ff0d758bbfe8a01
DenizaLoshi <deniza.loshi@web.de>
2024-03-25 17:06:34 +0100

    weather api

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let apiKey = '3d26b7e297cd652d53117069d6791786';
+    // let city = 'London';
+    let searcher = document.getElementById('searcher');
+
+    document.getElementById('sumbiti').addEventListener('click', () => {
+        let city = searcher.value.trim();
+
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                let weatherInfo = document.querySelector('.weather-info');
+                let temperatura = data.main.temp;
+                let description = data.weather[0].description;
+                let icon = data.weather[0].icon;
+                let wind = data.wind.speed;
+                weatherInfo.innerHTML = `
+                <h2>${city}</h2>
+                <img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather-icon">
+                <p>${description}</p>
+                <p>Temperature: ${temperatura}°C</p>
+            `;
+            
+            }).catch(error => {
+                console.error('Error fetching weather data:', error);
+                const weatherInfo = document.querySelector('.weather-info');
+                weatherInfo.innerHTML = '<p>Ate qe po kerkoni nuk gjendet</p>';
+            });
+    });
+});
